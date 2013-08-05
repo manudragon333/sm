be888e8c55137ed9830ceca9fb46d1ce0e9a77b5
School Mgmt <manudragon333@gmail.com>
2013-08-06 00:38:45 +0530

    Initial Commit.

---
+++ application/views/admin/userAdded.php
@@ -0,0 +1,3 @@
+<br/><p> User saved successfully.</p><br/><input type="button" name="imageField" id="imageField" class="send grey button" value="Back" 
+onclick="javascript:window.history.go(-2);"/>
+
